3335e7a09b01d434d4042aa8dbd81076192cca41
avahoffman <avamariehoffman@gmail.com>
2026-05-21 14:14:25 -0400

    Rename to slides, and allow drive pdf links

--- scripts/render_youtube_list.R
+++ scripts/render_youtube_list.R
@@ -31,18 +31,18 @@ make_youtube_table <- function(infile) {
         mutate(Date = format(contentDetails.videoPublishedAt, "%Y-%m-%d"))
         
       # Extract slides link
-      df$`Google Slides` <-
+      df$Slides <-
         df$snippet.description %>% 
-        str_extract_all("https://docs.google.com/presentation/d/.*") %>%
+        str_extract_all("https://docs.google.com/presentation/d/.*|https://drive.google.com/file/d/.*") %>%
         str_remove_all("\\.$") # Remove trailing period
       
       # Create Google Slides link w/ markdown magic
       df <- df %>% 
-        mutate(`Google Slides` = paste0("[Go to slides](", `Google Slides`, ")")) %>% 
-        select(Video, Date, `Google Slides`) %>% 
-        mutate(`Google Slides` = case_when(`Google Slides` == "[Go to slides](character(0))" ~ "N/A", # Often, there are no slides on the video description
-                                           `Google Slides` == "[Go to slides](NA)" ~ "N/A",
-                                           TRUE ~ `Google Slides`))
+        mutate(Slides = paste0("[Go to slides](", Slides, ")")) %>% 
+        select(Video, Date, Slides) %>% 
+        mutate(Slides = case_when(Slides == "[Go to slides](character(0))" ~ "N/A", # Often, there are no slides on the video description
+                                  Slides == "[Go to slides](NA)" ~ "N/A",
+                                  TRUE ~ Slides))
       
       # Remove duplicates if necessary
       df <- distinct(df) %>% 
